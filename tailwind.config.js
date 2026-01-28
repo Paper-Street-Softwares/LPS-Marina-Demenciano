aeafb69a29d6d516a6fda856b72872a64063a86c
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-28 14:03:42 -0300

    fix: Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,7 +33,7 @@ export default {
 
         // hex botãoColor wpp
         wppLight: '#075e54',
-        wppDark: '#3B9432',
+        wppDark: '#2E7027',
 
         // Minitags
         bgIcons: '#E0E2E9',
